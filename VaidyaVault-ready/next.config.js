384841c9cee249cb16f84194e73909a5f5f06ce6
doggypikachu <48470401+doggypikachu@users.noreply.github.com>
2025-10-06 20:25:00 -0500

    Delete VaidyaVault-ready directory

--- VaidyaVault-ready/next.config.js
+++
@@ -1,9 +0,0 @@
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: process.env.NEXT_BASE_PATH || '/VaidyaVault',
-  assetPrefix: process.env.NEXT_ASSET_PREFIX || '/VaidyaVault',
-};
-export default nextConfig;
